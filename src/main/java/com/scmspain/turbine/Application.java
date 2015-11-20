5584625ea71fab40d337f70773caddf86f85a2aa
Alberto <alberto@aramirez.es>
2015-11-20 21:25:36 +0100

    - Add docker-compose. - Compose application with Eureka. - Enable Eureka discovery. - Homepage

--- src/main/java/com/scmspain/turbine/Application.java
+++ src/main/java/com/scmspain/turbine/Application.java
@@ -1,21 +1,31 @@
 package com.scmspain.turbine;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
+@ComponentScan
 @EnableAutoConfiguration
+@EnableDiscoveryClient
+@RestController
 @EnableHystrixDashboard
 @EnableTurbine
-@ComponentScan
 public class Application {
+  @RequestMapping("/")
+  public String home() {
+    return "Hello world";
+  }
+
   public static void main(String[] args) {
-//    SpringApplication.run(Application.class, args);
-    new SpringApplicationBuilder(Application.class).web(true).run(args);
+    new SpringApplicationBuilder(Application.class)
+      .web(true)
+      .run(args);
   }
 }
