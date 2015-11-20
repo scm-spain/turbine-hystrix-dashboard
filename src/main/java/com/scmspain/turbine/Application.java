c2f3aa78bbf4616f7012c2cd28edefc9974980da
Alberto <alberto@aramirez.es>
2015-11-20 18:36:17 +0100

    Enable turbine

--- src/main/java/com/scmspain/turbine/Application.java
+++ src/main/java/com/scmspain/turbine/Application.java
@@ -2,14 +2,20 @@ package com.scmspain.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableHystrixDashboard
+@EnableTurbine
+@ComponentScan
 public class Application {
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+//    SpringApplication.run(Application.class, args);
+    new SpringApplicationBuilder(Application.class).web(true).run(args);
   }
 }
